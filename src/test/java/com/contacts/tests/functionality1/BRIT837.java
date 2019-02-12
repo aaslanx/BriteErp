83ef1cb6fc23a6b856ad26f87d0f4efe5987acd7
aaslanx <aaslanx@gmail.com>
2019-02-12 15:05:22 -0500

    Retryfailed tests added

--- src/test/java/com/contacts/tests/functionality1/BRIT837.java
+++ src/test/java/com/contacts/tests/functionality1/BRIT837.java
@@ -8,37 +8,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BRIT837 extends TestBase {
-//
-//    @Test
-//    public void BRIT_837(){
-//
-//            extentLogger = report.createTest("Verify that sales manager user should be able click on plus sign for direct messages");
-//
-//            extentLogger.info("1. Open url");
-//            Driver.getDriver().get(ConfigurationReader.getProperties("url"));
-//
-//            extentLogger.info("2. Click BriteErpDemo button");
-//            extentLogger.info("Expected result: clickable");
-//            pages.homePage().briteErpDemoButton.click();
-//
-//            extentLogger.info("3. Enter valid username and password and click Log in button");
-//            extentLogger.info("Expected result: System should display login button successfully");
-//            pages.homePage().email_box.sendKeys(ConfigurationReader.getProperties("usernameManager"));
-//            pages.homePage().password_box. sendKeys(ConfigurationReader.getProperties("passwordManager"));
-//
-//            pages.homePage().logInButton.click();
-//
-//            //wait(2);
-//            System.out.println(pages.homePage().contactsButton.getText() + " get text");
-//            System.out.println(pages.homePage().contactsButton.getAttribute("innerText").trim() + " get attribute");
-//
-//            Assert.assertEquals(pages.homePage().contactsButton.getAttribute("innerText").trim(), "Contacts");
-//                try {
-//                        Thread.sleep(2000);
-//                } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                }
-//
-//                pages.homePage().contactsButton.click();
-//    }
+
+    //-----------------------------|     TEST 10     |----------------------------------------------
+
+    @Test(testName = "BRIT 2914", priority = 10)
+    public void createCompany() {
+
+       Assert.fail();
+
+
+    }
+
 }
